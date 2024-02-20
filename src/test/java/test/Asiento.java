d88d623b08034673d9ee82561c15d28660dc34fa
Jonathan <joosoriore@unal.edu.co>
2024-02-19 21:21:51 -0500

    Update Asiento.java

--- src/test/java/test/Asiento.java
+++ src/test/java/test/Asiento.java
@@ -4,7 +4,7 @@ public class Asiento {
 	String color;
 	int precio;
 	int registro;
-	void cambiar(String color) {
+	void cambiarColor(String color) {
 		if ("rojo".equals(color) || "verde".equals(color) || "amarillo".equals(color) || "negro".equals(color) || "blanco".equals(color)) {
 			this.color = color;
 		}
