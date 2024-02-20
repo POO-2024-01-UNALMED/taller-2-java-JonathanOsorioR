6fe3e7f0e63ce98473784fa2064e704618cd9c13
Jonathan <joosoriore@unal.edu.co>
2024-02-19 21:51:57 -0500

    12

--- src/test/java/test/Auto.java
+++ src/test/java/test/Auto.java
@@ -19,23 +19,16 @@ public class Auto {
 		return totalAsientos;
 	}
 	String verificarIntegridad(){
-        String verificador="si";
-        if (motor.registro==this.registro){
-            for (int i = 0; i < asientos.length; i++) {
-                 if (asientos[i] != null){
-                    if (asientos[i].registro != this.registro){
-                        verificador="no";
-                        break;
-                    }
-                }
-            }
-        }else {
-            verificador="no";
-        }
-        if (verificador.equals("si")){
-            return "Auto original";
-        }else {
-            return "Las piezas no son originales";
-        }
-    }
+		if (motor.registro==registro){
+			for (int i=0; i<asientos.length;i++) {
+				if (asientos[i].registro!=registro) {
+					return "Las piezas no son originales";
+				}
+			}
+		    return "Auto original";
+		}
+		else {
+			return "Las piezas no son originales";
+		}
+	}
 }
